945d082786509d38e2de6369d129603809c2af82
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-05-18 15:36:27 -0300

    PreEntrega2+RuedaSanchez

--- segunda-preentrega/ecommerce.js
+++ segunda-preentrega/ecommerce.js
@@ -5,7 +5,7 @@ class Producto{
         this.nombre = nombre;
         this.precio = precio;
         this.stock = stock;
-        this .id = id;
+        this.id = id;
         this.tipo = tipo;
     }
 }
@@ -23,7 +23,7 @@ const saludoInicial = (admin) => {
 /** Carga de productos **/
 
 const cargarProductos = (cantProductos) => {
-    const prods = [];     // Arreglo donde se va a almacenar los productos que se agregue a la tienda
+    let prods = [];     // Arreglo donde se va a almacenar los productos que se agregue a la tienda
     for(let i=0; i < cantProductos; i++){
         let nombreNuevoProducto = prompt('Ingrese el nombre del producto numero ' + (i+1));
         let precioNuevoProducto = parseInt(prompt('Ingrese el precio (solo numeros naturales sin signo $) del producto numero ' + (i+1)));
@@ -31,17 +31,18 @@ const cargarProductos = (cantProductos) => {
         let idNuevoProducto = prompt('Ingrese el id del producto numero ' + (i+1));
         let tipoNuevoProducto = prompt('Ingrese la categoria del producto numero ' + (i+1));
         const nuevoProducto = new Producto(nombreNuevoProducto, precioNuevoProducto, stockNuevoProducto, idNuevoProducto, tipoNuevoProducto);
-        productos.push(nuevoProducto);
+        prods.push(nuevoProducto);
     }
     return prods;
 }
 
-/** Carga exitosa de productos **/
+/** Carga exitosa de productos **/ 
 
 const cargaExitosa = (admin, productos, cantProductos) => {
     alert(`Felicitaciones ${admin}! Todos sus productos han sido cargados con exito. Presione enter para ver el listado de productos`);
     alert(JSON.stringify(productos, null, cantProductos+1));
 }
+
 
 /****************** FUNCIONALIDAD POR PARTE DEL CLIENTE ******************/
 
@@ -58,7 +59,7 @@ const saludar = (clientName) => {
 /** Filtrar productos por la categoria ingresada por el cliente **/
 
 const filtrarPor = (cat, prods) => {
-    const prodsEncontrados = prods;
+    let prodsEncontrados = prods;                       // Declaro al arreglo let porque lo voy a reasignar
     if(cat === "nombre"){
         prodsEncontrados = filtrarPorNombre(prods);
     } else if(cat === "precio"){
@@ -69,15 +70,15 @@ const filtrarPor = (cat, prods) => {
         prodsEncontrados = filtrarPorId(prods);
     } else if(cat === "tipo"){
         prodsEncontrados = filtrarPorTipo(prods);
-    }
+    } 
     return prodsEncontrados;
 }
 
 /** Subfiltrado de productos por nombre **/
 
 const filtrarPorNombre = (prods) => {
-    alert(`En esta seccion, puede optar por los siguientes filtros: \n 1) creciente: los productos se filtran alfabeticamente de menor a mayor \n 2) decreciente: los productos se filtran alfabeticamente de mayor a menor \n 3) nombre: los productos se filtran por un nombre en particular`);
-    const prodsFiltrados = prods;
+    alert(`En esta seccion, puede optar por los siguientes filtros: \n 1) creciente: los productos se ordenan alfabeticamente de menor a mayor \n 2) decreciente: los productos se ordenan alfabeticamente de mayor a menor \n 3) nombre: los productos se filtran por un nombre en particular`);
+    let prodsFiltrados = prods;
     let filtro = prompt("Ahora, ingrese el nombre del filtro que quiere aplicar y presione enter para continuar.");
     switch(filtro){
         case "creciente":
@@ -85,12 +86,12 @@ const filtrarPorNombre = (prods) => {
                 if(a.nombre > b.nombre){
                     return 1;
                 } 
-                if(a.name < b.name){
+                if(a.nombre < b.nombre){
                     return -1;
                 }
                 // a es igual a b
                 return 0;
-            });
+            })
             prodsFiltrados = prods;
             break;
             
@@ -100,12 +101,12 @@ const filtrarPorNombre = (prods) => {
                 if(a.nombre > b.nombre){
                     return -1;
                 } 
-                if(a.name < b.name){
+                if(a.nombre < b.nombre){
                     return 1;
                 }
                 // a es igual a b
                 return 0;
-            });
+            })
             prodsFiltrados = prods;
             break;
 
@@ -125,11 +126,11 @@ const filtrarPorNombre = (prods) => {
 
 const filtrarPorPrecio = (prods) => {
     let valor = parseInt(prompt("Primero, ingrese el valor por el desea filtrar."));
-    let limite = prompt("Ahora, ingrese el limite (maximo, minimo o igual precio).");
-    const prodsFiltrados = prods;
-    if(limite === "maximo"){
+    let limite = prompt("Ahora, ingrese el limite (mayor, menor o igual precio).");
+    let prodsFiltrados = prods;
+    if(limite === "mayor"){
         prodsFiltrados = prods.filter(products => products.precio > valor);
-    } else if(limite === "minimo"){
+    } else if(limite === "menor"){
         prodsFiltrados = prods.filter(products => products.precio < valor);
     } else if(limite === "igual"){
         prodsFiltrados = prods.filter(products => products.precio === valor);
@@ -144,7 +145,7 @@ const filtrarPorPrecio = (prods) => {
 const filtrarPorStock = (prods) => {
     let cantStock = parseInt(prompt("Ingrese el numero de stock por el desea filtrar."));
     let limite = prompt("Ahora, ingrese el limite (maximo, minimo o igual stock).");
-    const prodsFiltrados = prods;
+    let prodsFiltrados = prods;
     if(limite === "maximo"){
         prodsFiltrados = prods.filter(products => products.stock > cantStock);
     } else if(limite === "minimo"){
@@ -162,7 +163,7 @@ const filtrarPorStock = (prods) => {
 const filtrarPorId = (prods) => {
     let identificador = parseInt(prompt("Ingrese el numero de id por el desea filtrar."));
     let operacion = prompt("Ahora, ingrese la operacion (mayor, menor o igual) a aplicar.");
-    const prodsFiltrados = prods;
+    let prodsFiltrados = prods;
     if(operacion === "mayor"){
         prodsFiltrados = prods.filter(products => products.id > identificador);
     } else if(operacion === "menor"){
@@ -180,7 +181,7 @@ const filtrarPorId = (prods) => {
 const filtrarPorTipo = (prods) => {
     alert(`En esta seccion, puede optar por los siguientes filtros: \n 1) herramientas: se muestran todos los productos de tipo herramienta \n 2) insumos: se muestran todos los productos de tipo insumo \n 3) workshops: se muestran todos los productos de tipo workshop \n 4) seminarios: se muestran todos los productos de tipo seminario \n 5) cursos: se muestran todos los productos de tipo curso`);
     let tipo = prompt("Ahora, ingrese el tipo del producto.");
-    const prodsFiltrados = prods;
+    let prodsFiltrados = prods;
     switch(tipo){
         case "herramientas":
             prodsFiltrados = prods.filter(products => products.tipo === "herramientas");            
@@ -199,7 +200,7 @@ const filtrarPorTipo = (prods) => {
             break;
                         
         case "cursos":
-            prodsFiltrados = productos.filter(products => products.tipo === "cursos");            
+            prodsFiltrados = prods.filter(products => products.tipo === "cursos");            
             break;
             
         default:
@@ -211,36 +212,47 @@ const filtrarPorTipo = (prods) => {
 
 /** Buscar la informacion de un producto por el nombre **/
 
-const buscar = (prodsEncontrados, nombreProd) => {
+const buscarStock = (prodsEncontrados, nombreProd) => {
+    let stockDisponible;
     for(const item of prodsEncontrados){
         if(item.nombre === nombreProd){
-            const infoProd = item;
+            stockDisponible = item.stock;
         }
     }
-    return infoProd;
+    return stockDisponible;
+}
+
+const buscarPrecio = (prodsEncontrados, nombreProd) => {
+    let importe;
+    for(const item of prodsEncontrados){
+        if(item.nombre === nombreProd){
+            importe = item.precio;
+        }
+    }
+    return importe;
 }
 
 /** Cobro efectuado con tarjeta de credito */
 
-const cobrarConTarjetaCredito = (nombre, cantidad, precio, pago) => {
+const cobrarConTarjetaCredito = (nombre, cantidad, precio) => {
     alert(`Ha elegido abonar su pedido con Tarjeta de credito. \n Al abonar con este medio de pago tiene un recargo del 30% y hasta 6 cuotas sin interes`);
+    let precioRegular = precio * cantidad;
     let recargo = precioRegular * 0.3;
-    precioFinal = precioRegular + recargo;
+    let precioFinal = precioRegular + recargo;
     alert(`El costo regular de su pedido de ${cantidad} unidades de ${nombre} es de $${precioRegular}. \n Con este medio de pago, el recargo es de $${recargo} y el valor final es de $${precioFinal}. \n Presione enter para continuar la compra.`);
     let cantCuotas = parseInt(prompt("Ahora, ingrese la cantidad de cuotas en las que desea abonar su pedido (entre 1 y 12 inclusive)"));
     if((cantCuotas >= 1) && (cantCuotas <= 6)){
-        let valorCuota = precioFinal / cantCuotas;
+        let valorCuota = (precioFinal / cantCuotas).toFixed(3);     // Truncamiento a tres (3) decimales
         alert(`Estimado cliente. Usted va a abonar ${cantCuotas} fijas sin intereses de $${valorCuota}. \n Presione enter para confirmar la compra.`);
     } else if((cantCuotas > 6) && (cantCuotas <= 12)){
         alert(`Estimado cliente. Al elegir ${cantCuotas} cuotas, se le cobrara un recargo adicional del 5% por impuestos de tarjeta sobre el valor final de su pedido. \n Presione enter para continuar.`);
         let impuesto = precioFinal * 0.05;
         precioFinal += impuesto;
-        valorCuota = precioFinal / cantCuotas;
+        valorCuota = (precioFinal / cantCuotas).toFixed(3);
         alert(`Estimado cliente. Usted va a abonar ${cantCuotas} fijas de $${valorCuota}. \n Presione enter para confirmar la compra.`);
     } else {
         alert(`Numero de cuotas incorrecto. Por favor, ingrese un numero entre 1 y 12 inclusive e intente nuevamente.`);
     }
-
 }
 
 /** Realizar el cobro del producto **/
@@ -266,7 +278,7 @@ const tramitarPagoSegun = (nombre, cantidad, precio, pago) => {
             break;
 
         case "Tarjeta de credito":
-            cobrarConTarjetaCredito(nombre, cantidad, precio, pago);     
+            cobrarConTarjetaCredito(nombre, cantidad, precio);     
             break;
             
         case "Criptomonedas":
@@ -296,9 +308,21 @@ const informarRetiroPedido = (medioPago) => {
     }
 }
 
+/** Proceso de borrar un producto del sistema **/
+
+const borrarDelSistema = (prods, nombreProducto) => {
+    let indice = -1;                                     // Inicializacion en -1 pero el elemento se encuentra en el arreglo
+    for(let i=0; i < prods.length; i++){
+        if(prods[i].nombre === nombreProducto){
+            indice = i;
+            prods.splice(indice,1);
+        }
+    }
+}
+
 /** Proceso de efectuar compra por parte del cliente **/
 
-const realizarCompra = (prodsEncontrados) => {
+const realizarCompra = (prodsEncontrados, prods) => {
     if(prodsEncontrados.length === 0){
         // No hay productos disponibles con el filtro aplicado
         alert(`Ups! Lo sentimos mucho. No se encontraron productos disponibles con el filtro seleccionado. Recargue la pagina para comenzar nuevamente con busqueda`);
@@ -312,13 +336,17 @@ const realizarCompra = (prodsEncontrados) => {
         alert(nombreProductosEncontrados);
         let nombreProductoComprar = prompt("Ahora, ingrese el nombre del producto que desea adquirir");
         let cantidadProductoComprar = parseInt(prompt('Ingrese la cantidad de ' + nombreProductoComprar + ' que desea comprar'));
-        const infoProductoElegido = buscar(prodsEncontrados, nombreProductoComprar);
-        if(infoProductoElegido.stock >= cantidadProductoComprar){
+        let stockProductoElegido = buscarStock(prodsEncontrados, nombreProductoComprar);
+        let precioProductoElegido = buscarPrecio(prodsEncontrados, nombreProductoComprar);
+        if(stockProductoElegido >= cantidadProductoComprar){
             // Hay suficiente stock para ejecutar la compra
-            infoProductoElegido.stock = infoProductoElegido.stock - cantidadProductoComprar; // Actualizacion del stock
+            stockProductoElegido = stockProductoElegido - cantidadProductoComprar; // Actualizacion del stock
+            if(stockProductoElegido === 0){
+                // Borrar al producto del sistema
+                borrarDelSistema(prods, nombreProductoComprar);
+            }
             alert(`Excelente! Contamos con stock suficiente de ${nombreProductoComprar} para satisfacer su pedido. Presione enter para comenzar el proceso de pago del pedido realizado.`);
             alert(`Contamos con los siguientes metodos de pago: \n 1) Transferencia bancaria \n 2) Tarjeta de credito \n 3) Mercadopago \n 4) Criptomonedas`);
-            let precioProductoElegido = infoProductoElegido.precio;
             let formaPago = prompt("Por favor, ingrese el medio elegido");
             tramitarPagoSegun(nombreProductoComprar, cantidadProductoComprar, precioProductoElegido, formaPago);
             informarRetiroPedido(formaPago); 
@@ -337,7 +365,7 @@ const realizarCompra = (prodsEncontrados) => {
 let administrador = prompt("Bienvenido al administrador e-commerce emergente de Espacio Arcoiris Crear. Ingrese su nombre para continuar.");
 saludoInicial(administrador);
 let cantProductos = parseInt(prompt(administrador + ' ingrese la cantidad de productos que desea cargar en el sistema.'));
-const productos = cargarProductos(cantProductos);
+let productos = cargarProductos(cantProductos);
 cargaExitosa(administrador, productos, cantProductos);
 
 // Seccion Cliente
@@ -345,5 +373,5 @@ let nombreCliente = prompt("Ahora, vamos a comenzar con el proceso de compra de 
 saludar(nombreCliente);
 let categoria = prompt(nombreCliente + ' , ingrese el nombre de la categoria por la que desea filtrar (nombre, precio, stock, id o tipo) o presione cualquier tecla si desea ver el listado completo de productos.');
 const productosEncontrados = filtrarPor(categoria, productos);
-realizarCompra(productosEncontrados);
+realizarCompra(productosEncontrados, productos);
 
